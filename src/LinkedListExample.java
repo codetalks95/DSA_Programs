068302603cf11536fda36989b623c0732f6c5cbf
codetalks95 <info@codetalksdna.com>
2023-06-11 23:41:11 +0530

    Added new Methods in LinkedList Code.

--- src/LinkedListExample.java
+++ src/LinkedListExample.java
@@ -30,7 +30,7 @@ public class LinkedListExample {
         length++;
     }
 
-    public Node removeFirst() {
+    public Node removeFromFirst() {
         Node temp;
         if (head == null) {
             return null;
@@ -43,6 +43,112 @@ public class LinkedListExample {
             return null;
         }
         return temp;
+    }
+
+    public Node removeFromLast() {
+        if (length == 0) {
+            return null;
+        }
+        Node temp = head;
+        Node pre = head;
+
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        if (index == 0) {
+            removeFromFirst();
+        }
+        if (index == length - 1) {
+            removeFromLast();
+        }
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+        length--;
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public void reverse() {
+        Node current = head;
+        Node previous = null;
+        Node next;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        tail = head;
+        head = previous;
+    }
+
+    public boolean set(int index, int value) {
+        if (index < 0 || index >= length) {
+            return false;
+        }
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) {
+            return false;
+        }
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
     }
 
     public void printList() {
@@ -62,10 +168,23 @@ public class LinkedListExample {
     }
 
     public static void main(String[] args) {
-        LinkedListExample linkedListExample = new LinkedListExample(4);
+        LinkedListExample linkedListExample = new LinkedListExample(1);
+        linkedListExample.append(2);
+        linkedListExample.append(3);
+        linkedListExample.append(4);
         System.out.println("Length of the LinkedList is" + " : " + linkedListExample.length);
-        linkedListExample.append(20);
-        linkedListExample.removeFirst();
+        linkedListExample.getHead();
+        linkedListExample.getTail();
+        linkedListExample.removeFromFirst();
+        linkedListExample.printList();
+        System.out.println(System.lineSeparator() + "LinkedList Value at Index" + " : " + linkedListExample.get(2).value);
+        System.out.println("Boolean Value for the set method" + " " + linkedListExample.set(2, 5));
+        linkedListExample.prepend(10);
+        linkedListExample.insert(2, 20);
+        linkedListExample.printList();
+        System.out.println(System.lineSeparator() + "Elements Removed from the last" + " " + linkedListExample.removeFromLast().value);
+        System.out.println(System.lineSeparator() + "Elements Removed from the LinkedList" + " " + linkedListExample.remove(2).value);
+        linkedListExample.reverse();
         linkedListExample.printList();
     }
 }
